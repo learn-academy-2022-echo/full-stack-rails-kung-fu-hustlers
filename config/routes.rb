7d4faabd2058e1f9eed656d9bc79b940d7ee42d4
sanjuel <samueljscott101@gmail.com>
2022-09-07 15:19:42 -0700

    finished index, show, new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "blog#index"
+  get 'blogs' => 'blog#index', as: 'blogs'
+  get 'blogs/new' => 'blog#new', as: 'new_blog'
+  get 'blogs/:id' => 'blog#show', as: 'blog'
+  post 'blogs' => 'blog#create'
 end
