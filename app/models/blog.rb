bf80ba6691ba8cd92f9b7277f1e70ccee93a305c
namieralsabagh <namier@me.com>
2022-09-08 10:24:06 -0700

    Stretch

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
     validates :title, :content, presence: true
+    validates :title, length: { minimum: 10 }
+    validates :title, uniqueness: true 
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it 'Is not valid without a title'
+ blog = Blog.create title: nil, content: 'asdfg'
+ expect(blog.errors[:title]).to_not be_empty
 end
