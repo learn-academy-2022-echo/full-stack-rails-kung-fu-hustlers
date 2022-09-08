121d280bf5f6a7479db9910a64ebb5f690b1cb7f
namieralsabagh <namier@me.com>
2022-09-08 10:08:34 -0700

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'blogs/new' => 'blog#new', as: 'new_blog'
   get 'blogs/:id' => 'blog#show', as: 'blog'
   post 'blogs' => 'blog#create'
+  get 'blogs/edit/:id' => 'blog#edit', as: 'edit_blog'
+  delete '/blogs/:id' => 'blog#destroy', as: 'delete_blog' 
+  patch '/blogs/:id' => 'blog#update', as: 'patch_blog'
 end
